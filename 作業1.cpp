920c1f6360df1645e5a9b56af9024ed3919bcbaa
411211223JerryWang <147466236+411211223JerryWang@users.noreply.github.com>
2023-10-11 09:42:17 +0800

    Add files via upload

--- 作業1.cpp
+++ 作業1.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
-int main(){
+int main() {
     int i,j;
-    for (i=1, i<=9, i++){
-        for(j=1, j<=9, j++){
+    for(i=1; i<=9; i++){
+        for(j=1; j<=9; j++){
             printf("%d*%d=%2d\t", i, j, i*j);
         }
         printf("\n");
